6e98a45740af9e9cb1ddb08b61595c9c014dbcc1
Geyslan G. Bem <geyslan@gmail.com>
2021-12-06 19:18:19 -0300

    bootloader: first implementation
    Set a zeroed BIOS parameter block, set the segment registers, setup
    the stack and finally calls a rough print implementation with a hello world.

---
+++ boot.asm
@@ -0,0 +1,60 @@
+org 0
+bits 16
+
+; bpb (bios parameter block)
+bpb:
+	jmp short setup1
+	nop
+; just reserve space, as some BIOSes may change this area
+times 33 db 0
+
+; BIOS historically loads the bootloader to 0x7c00 memory address (20 bits)
+; cs:offset is translated to ((cs << 4) + offset) address
+; so 'jmp 0x07c0:setup':
+; - set code segment (cs) to 0x07c0
+; - jump to (0x7c00 + setup offset)
+setup1:
+	jmp 0x07c0:setup2
+
+setup2:
+	cli ; disable interrupt handlers
+
+	; set data segment (ds) equal to code segment (cs)
+	mov ax, cs
+	mov ds, ax	
+
+	; setup stack
+	mov ax, 0
+	mov ss, ax
+	; the beginning of the stack is the same as the code in memory
+	; but the stack grows downwards
+	mov sp, 0x7c00
+
+	sti ; enable interrupt handlers
+
+start:
+	; call print
+	mov si, message
+	call print
+	jmp $
+
+print:
+	mov bx, 0
+.loop:
+	lodsb
+	cmp al, 0
+	je .done
+	call print_char
+	jmp .loop
+.done:
+	ret
+
+print_char:
+	mov ah, 0xe
+	int 0x10
+	ret
+
+message: db 'Hello World! I am GregOS Bootloader!', 0
+
+times 510-($ - $$) db 0
+dw 0xaa55
