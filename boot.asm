354d5e29f167e962ab3c1f036ad3d05a022b5846
Geyslan G. Bem <geyslan@gmail.com>
2021-12-12 15:24:15 -0300

    bootloader: get rid of BIOS calls and go protected

--- boot.asm
+++ boot.asm
@@ -1,5 +1,8 @@
-org 0
+org 0x7c00
 bits 16
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
 
 ; FAT Boot Record
 ; BPB (BIOS Parameter Block)
@@ -16,19 +19,19 @@ times 62-($ - $$) db 0
 ; cs:offset is translated to ((cs << 4) + offset) address
 ; so 'jmp 0x07c0:setup':
 ; - set code segment (cs) to 0x07c0
-; - jump to (0x7c00 + setup offset)
+; - jump to (0x7c00 + setup2 offset)
 setup1:
-	jmp 0x07c0:setup2
+	jmp 0:setup2
 
 setup2:
 	cli ; disable interrupt handlers
 
-	; set data segment (ds) equal to code segment (cs)
+	; set data (ds) and extra (es) segments equal to code segment (cs)
 	mov ax, cs
-	mov ds, ax	
+	mov ds, ax
+	mov es, ax
 
-	; setup stack
-	mov ax, 0
+	; setup stack	
 	mov ss, ax
 	; the beginning of the stack is the same as the code in memory
 	; but the stack grows downwards
@@ -36,29 +39,63 @@ setup2:
 
 	sti ; enable interrupt handlers
 
-start:
-	; call print
-	mov si, message
-	call print
+.load_protected:
+	cli
+	lgdt [gdt_descriptor]	; Load Global Descriptor Table register (gdtr)
+	mov eax, cr0
+	or eax, 1
+	mov cr0, eax
+
+	jmp CODE_SEG:run32
+	
+
+; GDT (Global Descriptor Table) entries
+gdt_start:
+
+; offset 0
+gdt_null:
+	dq 0
+
+; offset 8
+gdt_code:		; (cs) should point to this descriptor
+	dw 0xffff	; segment limit first 0-15 bits
+	dw 0		; base low 0-15 bits
+	db 0		; base middle 16-23 bits
+	db 0x9a		; access byte
+	db 11001111b	; high 4 bit flags and the low 4 bit flags
+	db 0		; base high 24-31 bits
+
+; offset 16
+gdt_data:		; (ds, ss, es, fs, gs) should point to this descriptor
+	dw 0xffff	; segment limit first 0-15 bits
+	dw 0		; base low 0-15 bits
+	db 0		; base middle 16-23 bits
+	db 0x92		; access byte
+	db 11001111b	; high 4 bit flags and the low 4 bit flags
+	db 0		; base high 24-31 bits
+
+gdt_end:
+
+; GDT escriptor structure
+gdt_descriptor:
+	dw gdt_end - gdt_start - 1	; size - 1
+	dd gdt_start			; offset (linear address)
+
+
+; protected mode (32 bits) code 
+[bits 32]
+run32:
+	;; set segment registers
+	mov ax, DATA_SEG
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	mov ss, ax
+	mov ebp, 0x00200000
+	mov esp, ebp
+
 	jmp $
-
-print:
-	mov bx, 0
-.loop:
-	lodsb
-	cmp al, 0
-	je .done
-	call print_char
-	jmp .loop
-.done:
-	ret
-
-print_char:
-	mov ah, 0xe
-	int 0x10
-	ret
-
-message: db 'Hello World! I am GregOS Bootloader!', 0
 
 times 510-($ - $$) db 0
 dw 0xaa55
